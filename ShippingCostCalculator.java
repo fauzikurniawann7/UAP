953b8b2209a16b7047352c4ad562d191edda747b
fauzikurniawann7 <fauzikurniawan@student.ub.ac.id>
2025-05-31 21:25:04 +0700

    Delete ShippingCostCalculator.java

--- ShippingCostCalculator.java
+++
@@ -1,8 +0,0 @@
-package uap.interfaces;
-public interface ShippingCostCalculator {
-    int PRICE_PER_KG = 2000; 
-    default int calculateCost(double weightInKg) {
-        int kg = (int) Math.ceil(weightInKg);
-        return kg * PRICE_PER_KG;
-    }
-}
